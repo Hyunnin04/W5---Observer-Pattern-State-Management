5643a49eec2ac0291321d08c0cdf75eb2cd1c2eb
Hyunnin04 <sasodanin04@gmail.com>
2025-03-13 17:22:01 +0700

    2. Provide the model to the app root widget using ChangeNotifierProvider

--- lib/EXERCISE-2/main.dart
+++ lib/EXERCISE-2/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'models/color_counters.dart';
 import 'screens/home_screen.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(body: HomeScreen()),
+    ChangeNotifierProvider(
+      create: (context) => ColorCounters(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(body: HomeScreen()),
+      ),
     ),
   );
 }
