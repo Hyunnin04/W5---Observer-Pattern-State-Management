813bcacd861440831177f3228c638532db6513e8
Hyunnin04 <sasodanin04@gmail.com>
2025-03-13 17:32:42 +0700

    5. Use the colorCounters to get or increment the colors

--- lib/EXERCISE-2/screens/statistics_screen.dart
+++ lib/EXERCISE-2/screens/statistics_screen.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../models/color_counters.dart';
 
 class StatisticsScreen extends StatelessWidget {
-  
+  const StatisticsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Statistics')),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Red Taps: $redTapCount', style: TextStyle(fontSize: 24)),
-            Text('Blue Taps: $blueTapCount', style: TextStyle(fontSize: 24)),
-          ],
+        child: Consumer<ColorCounters>(
+          builder: (context, colorCounters, child) => Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Red Taps: ${colorCounters.redCount}',
+                  style: TextStyle(fontSize: 24)),
+              Text('Blue Taps: ${colorCounters.blueCount}',
+                  style: TextStyle(fontSize: 24)),
+            ],
+          ),
         ),
       ),
     );
