20feb4778f0a246d7debc874d5256c001bb3ca5f
Hyunnin04 <sasodanin04@gmail.com>
2025-03-13 17:29:33 +0700

    4. Wrap the widget who needs the model with a consumerConsumer<ColorCounters>

--- lib/EXERCISE-2/screens/color_taps_screen.dart
+++ lib/EXERCISE-2/screens/color_taps_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import '../models/card_type.dart';
+import 'package:provider/provider.dart';
+import 'package:week_3_blabla_project/EXERCISE-2/models/card_type.dart';
+import '../models/color_counters.dart';
 import '../widgets/color_tap.dart';
 
-class ColorTapsScreen extends StatefulWidget {
+class ColorTapsScreen extends StatelessWidget {
+  const ColorTapsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -10,8 +13,22 @@ class ColorTapsScreen extends StatefulWidget {
       appBar: AppBar(title: Text('Color Taps')),
       body: Column(
         children: [
-          ColorTap(type: CardType.red, tapCount: redTapCount, onTap: onRedTap),
-          ColorTap(type: CardType.blue, tapCount: blueTapCount, onTap: onBlueTap),
+          Consumer<ColorCounters>(
+            builder: (context, colorCounters, child) => Column(
+              children: [
+                ColorTap(
+                  type: CardType.red,
+                  tapCount: colorCounters.redCount,
+                  onTap: colorCounters.incrementRed,
+                ),
+                ColorTap(
+                  type: CardType.blue,
+                  tapCount: colorCounters.blueCount,
+                  onTap: colorCounters.incrementBlue,
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
